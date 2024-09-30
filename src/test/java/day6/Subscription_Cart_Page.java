00481441f9779088cb6d40dd19f57e82ecdeb0dd
C <sakthivigneshc3@gmail.com>
2024-09-30 23:33:02 +0530

    Day_6 FirstFileAdded

---
+++ src/test/java/day6/Subscription_Cart_Page.java
@@ -0,0 +1,59 @@
+package day6;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Subscription_Cart_Page {
+	
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://automationexercise.com/");
+
+		// Verify the home page title
+		String Home_Page_Title = driver.getTitle();
+		Assert.assertEquals(Home_Page_Title, "Automation Exercise");
+		
+//		JavascriptExecutor js = (JavascriptExecutor) driver;
+//      js.executeScript("window.scrollBy(0,100)");
+
+		WebElement cartLink = driver.findElement(By.xpath("//div[@class=\"shop-menu pull-right\"]/ul/li[3]"));
+		cartLink.click();
+		
+		// Verify the text 'SUBSCRIPTION'
+		WebElement subscriptionText = driver.findElement(By.xpath("//div[@class=\"single-widget\"]/h2"));
+		String subscriptionTextValidation = subscriptionText.getText();
+		Assert.assertEquals(subscriptionTextValidation, "SUBSCRIPTION");
+		
+		/*
+		 Enter Email id and click arrow button
+		 */
+		WebElement subscriptionEmailId = driver.findElement(By.id("susbscribe_email"));
+		subscriptionEmailId.sendKeys("asdf@gmail.com");
+		
+		WebElement Button = driver.findElement(By.xpath("//button[@id=\"subscribe\"]"));
+		Button.click();
+
+		// verify the success message
+		WebElement subscriptionMsg = driver.findElement(By.xpath("//div[@id=\"success-subscribe\"]/div"));
+		String subscriptionMsgText = subscriptionMsg.getText();
+		Assert.assertEquals(subscriptionMsgText, "You have been successfully subscribed!");
+		driver.quit();
+
+		
+	}
+	
+
+}
+
+
+
+/*
+ 
+  //div[@class="single-products"]      --> 34
+  
+ */
